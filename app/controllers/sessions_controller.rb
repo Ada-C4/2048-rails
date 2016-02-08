fb9cd1ec64d401855513647c7e922f8b1d367357
Rebecca Tolmach <rmtolmach@gmail.com>
2016-02-08 15:48:43 -0800

    can login with facebook. yay

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def new
-    provider = params[:format]
-    redirect_to "/auth/facebook"
-  end
-
   def create
-    auth_hash = request.env['omniauth.auth']
-# create method for line below
-    @user = User.find_or_create_from_omniauth(auth_hash)
-    session[:user_id] = @user.id
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
     redirect_to root_path
   end
 
   def destroy
-    reset_session
+    session[:user_id] = nil
     redirect_to root_path
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "site#index"
   get "/auth/facebook"
   get "/auth/facebook/callback", to: "sessions#create"
+  get "signout", to: "sessions#destroy" 
+
 end
