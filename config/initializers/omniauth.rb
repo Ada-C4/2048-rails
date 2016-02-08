fae80db3ca228a7c5500533d18054e9c8d84ae45
Rebecca Tolmach <rmtolmach@gmail.com>
2016-02-08 15:00:33 -0800

    added gmes and routes for Oauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+# what is this?
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   root "site#index"
+  get "/auth/facebook"
+  get "/auth/facebook/callback" to: "sessions#create"
 end
