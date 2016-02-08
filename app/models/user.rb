81f9d84077d962c4cef0d7383864810cdade8664
Lauren Granger <lr.granger21@gmail.com>
2016-02-08 15:29:08 -0800

    find and create methods for new users in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ActiveRecord::Base
+  validates :username, :uid, :provider, presence: true
+
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      #user found
+      return user
+    else
+      #create new user
+      user = User.new
+      user.uid        = auth_hash["uid"]
+      user.provider   = auth_hash["provider"]
+      user.username   = auth_hash["info"]["name"]
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end
+
+  def self.create_from_omniauth(auth_hash)
+    # Create a user
+  end
 end
