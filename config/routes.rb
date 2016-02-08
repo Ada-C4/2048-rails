21a2fcc71d9df29667f10ef0c264a079fd0960ed
Dreedle <davis.audrey.lynn@gmail.com>
2016-02-08 15:39:30 -0800

    User has secure password. Deleting dev strategy callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
   root "site#index"
-  get 'login' => 'sessions#new'
-  get "/auth/:provider/callback" => 'sessions#create', as: :auth
-  post "/auth/developer/callback" => 'sessions#create', as: :developer_auth
-  delete 'logout' => 'sessions#destroy'
   post 'users' => 'users#create'
-  
+  get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
+  get "/auth/:provider/callback" => 'sessions#create', as: :auth
 end
