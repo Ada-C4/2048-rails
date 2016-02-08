2e4454baf5857b9f19f304c19b51f34c930650bd
Rebecca Tolmach <rmtolmach@gmail.com>
2016-02-08 15:23:54 -0800

    generated user model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-# what is this?
-OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
