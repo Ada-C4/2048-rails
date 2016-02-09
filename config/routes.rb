a76e1061502639a56f50906ff2eab147a31755bc
Daphne <dg1181@nyu.edu>
2016-02-09 11:35:36 -0800

    Created games controller and route to save game

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
   root 'site#index'
+
   get '/auth/facebook/callback' => 'sessions#create'
   get 'auth/failure' => '/'
   get 'signout' => 'sessions#destroy'
+
+  post 'save' => 'games#save'
 end
