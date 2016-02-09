03fcfe5e7a0a8124bcfb1a40bed8d59b02f365dc
Lauren Granger <lr.granger21@gmail.com>
2016-02-09 10:19:18 -0800

    game controller and comments for what the game functions should be

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,11 @@
+class GameController < ApplicationController
+
+#create a new instace of a game
+
+#update the status of a game to wong or lost , or save an ongoing game
+
+#delete an ongoing game
+
+#show all the games that someone has played
+
+end
