38061b837e3a8f737d76e5e065d7a268b6c8d7ea
Dreedle <davis.audrey.lynn@gmail.com>
2016-02-09 10:09:28 -0800

    Adding logout button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "site#index"
   post 'users' => 'users#create'
   get 'login' => 'sessions#new'
-  post 'login/' => 'sessions#create'
+  post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get "/auth/:provider/callback" => 'sessions#create', as: :auth
 end
