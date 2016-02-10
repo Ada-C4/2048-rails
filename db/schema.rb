065b2e05f2a16ff288bcdeda535b9064d9ce9728
Jennie Buechner <janina.buechner@gmail.com>
2016-02-10 10:48:35 -0800

    add gamestate field to game model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209183932) do
+ActiveRecord::Schema.define(version: 20160210184651) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "game_over"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "gamestate"
   end
 
   create_table "users", force: :cascade do |t|
