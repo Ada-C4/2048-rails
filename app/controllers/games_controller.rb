294b9e510596414ccbaaaac6563e5fb774c5209f
Logan McDonald <loganmcdona11@gmail.com>
2016-02-10 12:06:31 -0800

    made tables nicer and ordered by things

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @games = current_user.games
+    @games = current_user.games.order(:created_at).reverse
   end
 
   def show
