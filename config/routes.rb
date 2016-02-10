1a456024ce9a4a8591b02f2a9e42f6b46bcb8ccb
Logan McDonald <loganmcdona11@gmail.com>
2016-02-10 14:00:10 -0800

    cleaning up routes, added something to games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-
   root "site#index"
   post 'users' => 'users#create'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get '/auth/:provider/callback' => 'sessions#create', as: :auth
+
   post '/games/save' => 'games#save'
-  get '/games' => 'games#index'
-  get '/games/:id' => 'games#show'
-  resources :games, only: [:update, :destroy]
+  resources :games, only: [:index, :update, :destroy, :show]
   get '/leaderboard' => 'games#leaderboard'
 
 end
