1977d7e69e1f41f4a75d7b10c0e9a7a6a0af68cb
Daphne <dg1181@nyu.edu>
2016-02-10 15:16:37 -0800

    New game immediately updates game state server side

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     if auth_hash["uid"]
       @user = User.find_or_create_from_omniauth(auth_hash)
+      @user.games << Game.create
       if @user
         session[:user_id] = @user.id
         redirect_to root_path
