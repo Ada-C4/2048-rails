75769fbbd5630a88e545bd1a5d163bbea051aa78
noglows <jessica.noglows@outlook.com>
2016-02-10 15:39:16 -0800

    Working load game

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,38 @@
 window.requestAnimationFrame(function () {
   var game = new GameManager(4, KeyboardInputManager, HTMLActuator, ApiStorageManager);
 
+  $.ajax({
+    method: "GET",
+    url: "/games/get_user",
+  })
+  .done(function(data) {
+    $.ajax({
+      method: "GET",
+      url: "/load_games",
+      data: {user_id: data.id}
+    })
+    .done(function(response) {
+      for (i=0; i < response.length; i++) {
+        $("#game-list").append("<li><a class='load_game_button' data-id='"+ response[i].id + "' href='javascript:void(0)'>" + response[i].created_at + "</a></li>");
+      }
+      $(".load_game_button").click(function(){
+        // needs game id, and call setup(state)
+        console.log(this.attributes[1].value)
+        var id = this.attributes[1].value;
+        $.ajax({
+          method: "GET",
+          url: "/retrieve_game",
+          data: {game_id: id}
+        })
+        .done(function(response) {
+          console.log(response.state);
+          game.setup(response.state);
+        });
+
+      });
+    });
+  });
+
 
   $("#save-button").click(function() {
     state = game.storageManager.storage.gameState;
--- public/js/game_manager.js
+++ public/js/game_manager.js
@@ -34,40 +34,41 @@ GameManager.prototype.isGameTerminated = function () {
 
 // Set up the game
 GameManager.prototype.setup = function(gameState) {
-  var self = this;
-  $.ajax({
-    method: "GET",
-    url: "/games/get_user",
-  })
-  .done(function(data) {
-    $.ajax({
-      method: "GET",
-      url: "/load_games",
-      data: {user_id: data.id}
-    })
-    .done(function(response) {
-      for (i=0; i < response.length; i++) {
-        $("#game-list").append("<li><a class='load_game_button' data-id='"+ response[i].id + "' href='javascript:void(0)'>" + response[i].created_at + "</a></li>");
-      }
-      $(".load_game_button").click(function(){
-        // needs game id, and call setup(state)
-        var id = this.attributes[1].value;
-        $.ajax({
-          method: "GET",
-          url: "/retrieve_game",
-          data: {game_id: id}
-        })
-        .done(function(response) {
-          console.log(response.state)
-          self.setup(response.state)
-        });
+  // var self = this;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/games/get_user",
+  // })
+  // .done(function(data) {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/load_games",
+  //     data: {user_id: data.id}
+  //   })
+  //   .done(function(response) {
+  //     for (i=0; i < response.length; i++) {
+  //       $("#game-list").append("<li><a class='load_game_button' data-id='"+ response[i].id + "' href='javascript:void(0)'>" + response[i].created_at + "</a></li>");
+  //     }
+  //     $(".load_game_button").click(function(){
+  //       // needs game id, and call setup(state)
+  //       var id = this.attributes[1].value;
+  //       $.ajax({
+  //         method: "GET",
+  //         url: "/retrieve_game",
+  //         data: {game_id: id}
+  //       })
+  //       .done(function(response) {
+  //         console.log(response.state)
+  //         self.setup(response.state)
+  //       });
+  //
+  //     });
+  //   });
+  // });
 
-      });
-    });
-  });
 
-  gameState = JSON.parse(gameState);
   if (gameState) {
+    gameState = JSON.parse(gameState);
     var previousState = gameState
   } else {
     var previousState = this.storageManager.getGameState();
