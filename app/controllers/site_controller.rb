a933057cd02231042c19921bf5da6c03b4cceaf8
Logan McDonald <loganmcdona11@gmail.com>
2016-02-11 09:53:58 -0800

    moving to a single page app perhaps

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
   before_action :current_user
 
   def index
+    @games = current_user.games.order(:created_at).reverse
   end
   
 end
--- public/js/keyboard_input_manager.js
+++ public/js/keyboard_input_manager.js
@@ -91,54 +91,57 @@ KeyboardInputManager.prototype.listen = function () {
   var touchStartClientX, touchStartClientY;
   var gameContainer = document.getElementsByClassName("game-container")[0];
 
-  gameContainer.addEventListener(this.eventTouchstart, function (event) {
-    if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
-        event.targetTouches.length > 1) {
-      return; // Ignore if touching with more than 1 finger
-    }
+  if (gameContainer != null) {
+    gameContainer.addEventListener(this.eventTouchstart, function (event) {
+      if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
+          event.targetTouches.length > 1) {
+        return; // Ignore if touching with more than 1 finger
+      }
 
-    if (window.navigator.msPointerEnabled) {
-      touchStartClientX = event.pageX;
-      touchStartClientY = event.pageY;
-    } else {
-      touchStartClientX = event.touches[0].clientX;
-      touchStartClientY = event.touches[0].clientY;
-    }
+      if (window.navigator.msPointerEnabled) {
+        touchStartClientX = event.pageX;
+        touchStartClientY = event.pageY;
+      } else {
+        touchStartClientX = event.touches[0].clientX;
+        touchStartClientY = event.touches[0].clientY;
+      }
 
-    event.preventDefault();
-  });
+      event.preventDefault();
+    });
 
-  gameContainer.addEventListener(this.eventTouchmove, function (event) {
-    event.preventDefault();
-  });
 
-  gameContainer.addEventListener(this.eventTouchend, function (event) {
-    if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
-        event.targetTouches.length > 0) {
-      return; // Ignore if still touching with one or more fingers
-    }
+    gameContainer.addEventListener(this.eventTouchmove, function (event) {
+      event.preventDefault();
+    });
 
-    var touchEndClientX, touchEndClientY;
+    gameContainer.addEventListener(this.eventTouchend, function (event) {
+      if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
+          event.targetTouches.length > 0) {
+        return; // Ignore if still touching with one or more fingers
+      }
 
-    if (window.navigator.msPointerEnabled) {
-      touchEndClientX = event.pageX;
-      touchEndClientY = event.pageY;
-    } else {
-      touchEndClientX = event.changedTouches[0].clientX;
-      touchEndClientY = event.changedTouches[0].clientY;
-    }
+      var touchEndClientX, touchEndClientY;
 
-    var dx = touchEndClientX - touchStartClientX;
-    var absDx = Math.abs(dx);
+      if (window.navigator.msPointerEnabled) {
+        touchEndClientX = event.pageX;
+        touchEndClientY = event.pageY;
+      } else {
+        touchEndClientX = event.changedTouches[0].clientX;
+        touchEndClientY = event.changedTouches[0].clientY;
+      }
 
-    var dy = touchEndClientY - touchStartClientY;
-    var absDy = Math.abs(dy);
+      var dx = touchEndClientX - touchStartClientX;
+      var absDx = Math.abs(dx);
 
-    if (Math.max(absDx, absDy) > 10) {
-      // (right : left) : (down : up)
-      self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
-    }
-  });
+      var dy = touchEndClientY - touchStartClientY;
+      var absDy = Math.abs(dy);
+
+      if (Math.max(absDx, absDy) > 10) {
+        // (right : left) : (down : up)
+        self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
+      }
+    });
+  }
 };
 
 KeyboardInputManager.prototype.restart = function (event) {
