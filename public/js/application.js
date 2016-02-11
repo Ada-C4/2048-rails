c8259256586ece40d18220af0826057d50d48bdd
Jennie Buechner <janina.buechner@gmail.com>
2016-02-11 12:31:09 -0800

    fix load game click error on homepage load

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,18 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  function loadGameClickHandler(){
+    var url = "/game/" + $(this).data().gameid;
+    $.ajax(url, {
+      type: "POST"
+    })
+      .done(function(data) {
+        game.setup(data.gamestate);
+      })
+      .fail(function(data){
+        console.log("FAIL", data);
+      });
+  }
   $.ajax('/games')
 	    .done(function(htmlRes){
 	      $('#saved-games').html(htmlRes);
