620e86f3bfcbb802d120c2fbb20458bd3af46cf3
Jennie Buechner <janina.buechner@gmail.com>
2016-02-12 11:27:57 -0800

    complete delete game functionality

--- public/js/game_manager.js
+++ public/js/game_manager.js
@@ -22,6 +22,7 @@ GameManager.prototype.saveGameState = function () {
   var url = "/game",
       stringGameState = JSON.stringify(this.storageManager.getGameState()),
       game = this;
+  // load saved games
   function loadGameClickHandler(){
     var url = "/game/" + $(this).data().gameid;
     $.ajax(url, {
@@ -32,6 +33,22 @@ GameManager.prototype.saveGameState = function () {
       })
       .fail(function(data){
         console.log("FAIL", data);
+      });
+  }
+  // delete saved games
+  function deleteGameClickHandler(){
+    console.log('clicked!');
+    var gameId = $(this).data().gameid,
+        url = "/game/" + gameId;
+    $.ajax(url, {
+      type: "DELETE"
+    })
+      .done(function(data){
+        console.log(data);
+        $('#'+gameId).remove();
+      })
+      .fail(function(){
+        console.log('Deleted game FAIL', data);
       });
   }
   $.ajax(url, {
@@ -47,6 +64,7 @@ GameManager.prototype.saveGameState = function () {
             $('#saved-games').html(htmlRes);
             // bind load game handler to loadgame links
             $('.loadGame').click(loadGameClickHandler);
+            $('.deleteGame').click(deleteGameClickHandler);
           })
           .fail(function(){
             console.log('show user saved games: fail', html);
